a86f67ce0f890ef17b390f7cc14b5f2921b437f3
casey <caseythavy@gmail.com>
2020-06-06 17:02:37 -0700

    logo bounces w color

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d', {alpha: false});
+var dvd = new Image();
+dvd.src = "dvd.svg";
 
 var ball = {
-  x: 100,
-  y: 100,
+  x: 300,
+  y: 150,
   vx: 3,
   vy: 2,
-  yoffset: 25,
-  xoffset: 25,
-  radius: 25,
+  width: 300,
+  height: 150,
   color: 'blue',
   draw: function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    ctx.closePath();
+    
     ctx.fillStyle = this.color;
-    ctx.fill();
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.globalCompositeOperation = "destination-in";
+    ctx.drawImage(dvd, this.x, this.y, this.width, this.height);
+    
+    // ctx.fill();
     window.requestAnimationFrame(draw);
   },
   recolor: function() {
     this.color = "hsl(" + Math.floor(Math.random()*360) + ", 100%, " + Math.floor(Math.random()*50+30) + "%)";
     ctx.fillStyle = this.color;
+    
+    console.log(this.color);
   }
 };
 
 function draw() {
-  if (ball.y + ball.vy + ball.yoffset > canvas.height || ball.y + ball.vy - ball.yoffset < 0) {
+  if (ball.y + ball.vy + ball.height > canvas.height || ball.y + ball.vy < 0) {
     ball.vy = -ball.vy;
     ball.recolor();
   }
-  if (ball.x + ball.vx + ball.xoffset > canvas.width || ball.x + ball.vx - ball.xoffset < 0) {
+  if (ball.x + ball.vx + ball.width > canvas.width || ball.x + ball.vx < 0) {
     ball.vx = -ball.vx;
     ball.recolor();
   }
@@ -42,4 +47,4 @@ function draw() {
 
 // @todo make responsive
 
-ball.draw();
+dvd.onload = ball.draw();
